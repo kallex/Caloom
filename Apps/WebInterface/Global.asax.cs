71deffb1f1812eeef9f0de446dcb79ad9fa01d71
Kalle Launiala <kalle.launiala@protonit.net>
2012-08-21 11:00:10 +0300

    Updated the configuration reading from dynamic un-source controlled source

--- Apps/WebInterface/Global.asax.cs
+++ Apps/WebInterface/Global.asax.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
+using Microsoft.WindowsAzure;
+using TheBall;
 
 namespace WebInterface
 {
@@ -12,7 +15,13 @@ namespace WebInterface
 
         protected void Application_Start(object sender, EventArgs e)
         {
-
+            string connStr;
+            const string ConnStrFileName = @"C:\work\abs\ConnectionStringStorage\theballconnstr.txt";
+            if(File.Exists(ConnStrFileName))
+                connStr = File.ReadAllText(ConnStrFileName);
+            else
+                connStr = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            StorageSupport.InitializeWithConnectionString(connStr);
         }
 
         protected void Session_Start(object sender, EventArgs e)
