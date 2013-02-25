1c7ed9002a14aa0ca6fdf1d173ec395187107168
Kalle Launiala <kalle.launiala@protonit.net>
2013-02-25 08:00:38 +0200

    Image group changes for better usability (in progress)

--- Apps/AzureSupport/Partials/AddAddressAndLocationInfo.cs
+++ Apps/AzureSupport/Partials/AddAddressAndLocationInfo.cs
@@ -7,23 +7,6 @@ using TheBall.CORE;
 
 namespace AaltoGlobalImpact.OIP
 {
-    partial class AddImageInfo : IAddOperationProvider
-    {
-        public bool PerformAddOperation(string commandName, InformationSourceCollection sources, string requesterLocation, HttpFileCollection files)
-        {
-            if(ImageTitle == "")
-                throw new InvalidDataException("Image title is mandatory");
-            //IInformationObject container = sources.GetDefaultSource().RetrieveInformationObject();
-            Image image = Image.CreateDefault();
-            VirtualOwner owner = VirtualOwner.FigureOwner(this);
-            image.SetLocationAsOwnerContent(owner, image.ID);
-            image.Title = ImageTitle;
-            StorageSupport.StoreInformationMasterFirst(image, owner, true);
-            DefaultViewSupport.CreateDefaultViewRelativeToRequester(requesterLocation, image, owner);
-            return true;
-        }
-    }
-
     partial class AddImageGroupInfo : IAddOperationProvider
     {
         public bool PerformAddOperation(string commandName, InformationSourceCollection sources, string requesterLocation, HttpFileCollection files)
---
+++ Apps/AzureSupport/Partials/AddImageInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Web;
+using TheBall;
+using TheBall.CORE;
+
+namespace AaltoGlobalImpact.OIP
+{
+    partial class AddImageInfo : IAddOperationProvider
+    {
+        public bool PerformAddOperation(string commandName, InformationSourceCollection sources, string requesterLocation, HttpFileCollection files)
+        {
+            if(ImageTitle == "")
+                throw new InvalidDataException("Image title is mandatory");
+            //IInformationObject container = sources.GetDefaultSource().RetrieveInformationObject();
+            Image image = Image.CreateDefault();
+            VirtualOwner owner = VirtualOwner.FigureOwner(this);
+            image.SetLocationAsOwnerContent(owner, image.ID);
+            image.Title = ImageTitle;
+            StorageSupport.StoreInformationMasterFirst(image, owner, true);
+            DefaultViewSupport.CreateDefaultViewRelativeToRequester(requesterLocation, image, owner);
+            return true;
+        }
+    }
+}
