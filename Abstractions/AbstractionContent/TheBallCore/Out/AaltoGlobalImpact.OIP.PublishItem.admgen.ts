75da06540f7de286af650af5834c7da79aa48e4e
Kalle Launiala <kalle.launiala@protonit.net>
2013-05-02 16:27:29 +0300

    Updated to reflect UI generation parts

---
+++ Abstractions/AbstractionContent/TheBallCore/Out/AaltoGlobalImpact.OIP.PublishItem.admgen.ts
@@ -0,0 +1,13 @@
+ 
+
+class PublishItem {
+	Name: string;
+    testItem: Date;
+
+    constructor() {
+        var test2 = new Date();
+        this.testItem = test2;
+
+    }
+}
+
---
+++ Abstractions/AbstractionContent/TheBallCore/Out/AaltoGlobalImpact.OIP.PublishSummary.admgen.ts
@@ -0,0 +1,14 @@
+ 
+
+class PublishSummary {
+	ActiveItem: string;
+	PublishCollection: PublishItem[];
+    testItem: Date;
+
+    constructor() {
+        var test2 = new Date();
+        this.testItem = test2;
+
+    }
+}
+
