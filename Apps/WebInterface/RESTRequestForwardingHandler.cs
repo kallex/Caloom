7e9eaa61eb01b6142b114fa0c12c541468e9db11
Kalle Launiala <kalle.launiala@protonit.net>
2013-10-14 06:14:07 +0300

    Added stub for RESTRequestForwardingHandler (to proxy Azure REST requests with authorization/custom keys)

---
+++ Apps/WebInterface/RESTRequestForwardingHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Security;
+using AaltoGlobalImpact.OIP;
+using DotNetOpenAuth.OpenId.RelyingParty;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using TheBall;
+
+namespace WebInterface
+{
+    public class RESTRequestForwardingHandler : IHttpHandler
+    {
+        /// <summary>
+        /// You will need to configure this handler in the web.config file of your 
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: http://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            // Return false in case your Managed Handler cannot be reused for another request.
+            // Usually this would be false in case you have some state information preserved per request.
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string user = context.User.Identity.Name;
+            HttpRequest request = context.Request;
+        }
+
+        #endregion
+    }
+}
