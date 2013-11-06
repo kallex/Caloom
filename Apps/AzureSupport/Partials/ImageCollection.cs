aff3bdd63308993daefc4753d81432663ae840ae
Kalle Launiala <kalle.launiala@protonit.net>
2013-11-06 19:48:57 +0200

    Added .json format extension for binary and image collections

---
+++ Apps/AzureSupport/Partials/ImageCollection.cs
@@ -0,0 +1,15 @@
+﻿namespace AaltoGlobalImpact.OIP
+{
+    partial class ImageCollection : IAdditionalFormatProvider
+    {
+        AdditionalFormatContent[] IAdditionalFormatProvider.GetAdditionalContentToStore(string masterBlobETag)
+        {
+            return this.GetFormattedContentToStore(masterBlobETag, AdditionalFormatSupport.WebUIFormatExtensions);
+        }
+
+        string[] IAdditionalFormatProvider.GetAdditionalFormatExtensions()
+        {
+            return this.GetFormatExtensions(AdditionalFormatSupport.WebUIFormatExtensions);
+        }
+    }
+}
