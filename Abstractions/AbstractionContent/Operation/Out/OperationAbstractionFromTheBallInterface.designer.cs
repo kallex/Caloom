9f4c48169433047be960e3926b07dd728842658f
Kalle Launiala <kalle.launiala@protonit.net>
2014-01-20 11:19:05 +0200

    Added TheBall.Interface domain for web request separation from TheBall.CORE

---
+++ Abstractions/AbstractionContent/Operation/Out/OperationAbstractionFromTheBallInterface.designer.cs
@@ -0,0 +1,28 @@
+ 
+
+using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+		namespace TheBall.Infrastructure { 
+				public class ExecuteOperationParameters 
+		{
+				public string OwnerLocation ;
+				public string OperationDomain ;
+				public string OperationName ;
+				public byte[] OperationParameters ;
+				}
+		
+		public class ExecuteOperation 
+		{
+				private static void PrepareParameters(ExecuteOperationParameters parameters)
+		{
+					}
+				public static void Execute(ExecuteOperationParameters parameters)
+		{
+						PrepareParameters(parameters);
+					}
+				}
+		 } 
