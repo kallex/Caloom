521120293072c743fb1f2f5163e71c6a9d3c11d6
Kalle Launiala <kalle.launiala@protonit.net>
2014-04-11 11:46:55 +0300

    Fixed empty location object (and its latitude and longitude) on new creation

--- Apps/AzureSupport/Partials/AddressAndLocation.cs
+++ Apps/AzureSupport/Partials/AddressAndLocation.cs
@@ -8,6 +8,12 @@ namespace AaltoGlobalImpact.OIP
         {
             if (ReferenceToInformation == null)
                 ReferenceToInformation = ReferenceToInformation.CreateDefault();
+            if(this.Location == null)
+                this.Location = new Location();
+            if(this.Location.Latitude == null)
+                this.Location.Latitude = new Latitude();
+            if(this.Location.Longitude == null)
+                this.Location.Longitude = new Longitude();
             ReferenceToInformation.Title = this.Location.LocationName;
             ReferenceToInformation.URL = RelativeLocation; // DefaultViewSupport.GetDefaultViewURL(this);
             if (String.IsNullOrEmpty(Location.Latitude.TextValue))
