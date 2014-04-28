c14d094b9bedc03de698e686f21b004c04c50f63
Kalle Launiala <kalle.launiala@protonit.net>
2014-04-28 15:42:09 +0300

    TransferGroupInitiator untested implementation

--- Apps/AzureSupport/TheBall.CORE/TransferGroupInitiatorImplementation.cs
+++ Apps/AzureSupport/TheBall.CORE/TransferGroupInitiatorImplementation.cs
@@ -1,15 +1,56 @@
+using System.Linq;
+using AaltoGlobalImpact.OIP;
+
 namespace TheBall.CORE
 {
     public class TransferGroupInitiatorImplementation
     {
         public static void ExecuteMethod_AddNewInitiatorToGroup(string groupId, string newInitiatorAccountId)
         {
-            throw new System.NotImplementedException();
+            TBRGroupRoot groupRoot = TBRGroupRoot.RetrieveFromDefaultLocation(groupId);
+            TBRAccountRoot accountRoot = TBRAccountRoot.RetrieveFromDefaultLocation(newInitiatorAccountId);
+            var accountEmails = accountRoot.Account.Emails.CollectionContent;
+            var group = groupRoot.Group;
+            foreach(TBEmail email in accountEmails)
+            {
+                var groupRole = new TBCollaboratorRole()
+                {
+                    Email = email,
+                    Role = TBCollaboratorRole.InitiatorRoleValue,
+                    RoleStatus = TBCollaboratorRole.RoleStatusMemberValue,
+                };
+                group.Roles.CollectionContent.Add(groupRole);
+            }
+            groupRoot.StoreInformation();
+            RefreshAccountGroupMemberships.Execute(new RefreshAccountGroupMembershipsParameters
+                {
+                    GroupRoot = groupRoot,
+                    AccountID = newInitiatorAccountId
+                });
         }
 
         public static void ExecuteMethod_RemoveOldInitiatorFromGroup(string groupId, string oldInitiatorAccountId)
         {
-            throw new System.NotImplementedException();
+            TBRGroupRoot groupRoot = TBRGroupRoot.RetrieveFromDefaultLocation(groupId);
+            TBRAccountRoot accountRoot = TBRAccountRoot.RetrieveFromDefaultLocation(oldInitiatorAccountId);
+            var accountEmails = accountRoot.Account.Emails.CollectionContent;
+            var group = groupRoot.Group;
+            foreach (TBEmail email in accountEmails)
+            {
+                var groupRolesToRemove = group.Roles.CollectionContent.Where(role => role.Email.EmailAddress == email.EmailAddress &&
+                                                                                    role.Role == TBCollaboratorRole.InitiatorRoleValue).ToArray();
+                foreach (var groupRoleToRemove in groupRolesToRemove)
+                {
+                    group.Roles.CollectionContent.Remove(groupRoleToRemove);
+                }
+            }
+            groupRoot.StoreInformation();
+            RefreshAccountGroupMemberships.Execute(new RefreshAccountGroupMembershipsParameters
+            {
+                GroupRoot = groupRoot,
+                AccountID = oldInitiatorAccountId
+            });
+
         }
     }
 }
