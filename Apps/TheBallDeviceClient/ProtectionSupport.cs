d51c7e9f1b5e4dfdea566daac1c53da2e0af95e0
Kalle Launiala <kalle.launiala@protonit.net>
2014-05-14 10:05:57 +0300

    Fixed conditional System.Security reference on Android build

--- Apps/TheBallDeviceClient/ProtectionSupport.cs
+++ Apps/TheBallDeviceClient/ProtectionSupport.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography;
+using System;
 
 namespace TheBall.Support.DeviceClient
 {
@@ -6,12 +7,20 @@ namespace TheBall.Support.DeviceClient
     {
         public static byte[] Protect(byte[] dataToProtect)
         {
+#if !MONODROID
             return ProtectedData.Protect(dataToProtect, null, DataProtectionScope.CurrentUser);
+#else
+            throw new NotSupportedException("ProtectedData not supported");
+#endif
         }
 
         public static byte[] Unprotect(byte[] dataToUnprotect)
         {
+#if !MONODROID
             return ProtectedData.Unprotect(dataToUnprotect, null, DataProtectionScope.CurrentUser);
+#else
+            throw new NotSupportedException("ProtectedData not supported");
+#endif
         }
     }
 }
