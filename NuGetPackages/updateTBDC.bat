814fa2db48e50b79f023fb03637e0d9ed3bff23f
Kalle Launiala <kalle.launiala@protonit.net>
2014-05-16 07:01:47 +0300

    Fixed NuGet package to require .NET 3.5 instead of .NET 2.0 (not runtime, but framework specific)

--- NuGetPackages/updateTBDC.bat
+++ NuGetPackages/updateTBDC.bat
@@ -1,5 +1,5 @@
-copy ..\Apps\TheBallDeviceClient\bin\Debug\TheBallDeviceClient.dll TheBallDeviceClient\lib\net20\
-copy ..\Apps\TheBallDeviceClient\bin\Debug\JsonFx.dll TheBallDeviceClient\lib\net20\
+copy ..\Apps\TheBallDeviceClient\bin\Debug\TheBallDeviceClient.dll TheBallDeviceClient\lib\net35\
+copy ..\Apps\TheBallDeviceClient\bin\Debug\JsonFx.dll TheBallDeviceClient\lib\net35\
 copy ..\Apps\TheBallDeviceClient\bin\MonoDroidDebug\TheBallDeviceClient.dll TheBallDeviceClient\lib\MonoAndroid2.2\
 copy ..\Apps\TheBallDeviceClient\bin\MonoDroidDebug\JsonFx.dll TheBallDeviceClient\lib\MonoAndroid2.2\
 copy ..\Tools\ContentSyncTool\bin\debug\ContentSyncTool.exe TheBallDeviceClient\tools\
